d2a84e0b32bb53b297ce922068902f4936944674
Ali-Aref1 <ali14915@gmail.com>
2024-09-15 13:40:44 +0300

    small fix to run-app.bat

--- run-app.bat
+++ run-app.bat
@@ -1,13 +1,2 @@
-@echo off
-
-echo Checking if any new packages need to be installed...
-npm ci --dry-run >nul 2>&1
-if %ERRORLEVEL% neq 0 (
-    echo New or missing packages detected. Running npm install...
-    npm install
-) else (
-    echo All packages are already installed and up to date.
-)
-
 echo Starting the Chat App...
 start "Chat App" cmd /k "npm run dev"
